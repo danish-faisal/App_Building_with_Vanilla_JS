31bee5c3185055d9ce7534b36409a651a0777cc4
= <danishfaisal.wwe@gmail.com>
2021-10-23 18:41:57 +0530

    Select and Modify elements with Query Selector

--- js/script.js
+++ js/script.js
@@ -17,8 +17,10 @@
 	// get weather data when user clicks Forecast button, then add temp & conditions to view
 	$('.forecast-button').click(function (e) {
 		e.preventDefault();
-		const location = $('#location').val();
-		$('#location').val('');
+		// const location = $('#location').val();
+		const location = document.querySelector('#location').value;
+		// $('#location').val('');
+		document.querySelector('#location').value = '';
 		/*
 		$.get(url + location + '&appid=' + apiKey).done(function(response) {
 			updateUISuccess(response);
@@ -52,9 +54,11 @@
 			city: response.name
 		};
 
-		const $into = $('.conditions')[0];
+		// const $into = $('.conditions')[0];
+		const into = document.querySelector('.conditions');
 
-		ReactDOM.render(<Forecast {...state} />, $into);
+		// ReactDOM.render(<Forecast {...state} />, $into);
+		ReactDOM.render(<Forecast {...state} />, into);
 
 		function Forecast(props) {
 			return (
@@ -105,9 +109,11 @@
 			}
 		}
 
-		const $into = $('.activities')[0];
+		// const $into = $('.activities')[0];
+		const into = document.querySelector('.activities');
 
-		ReactDOM.render(<Activities {...state} />, $into);
+		// ReactDOM.render(<Activities {...state} />, $into);
+		ReactDOM.render(<Activities {...state} />, into);
 
 		function Activities(props) {
 			const activitiesList = props.activities.map(function (activity, index) {
